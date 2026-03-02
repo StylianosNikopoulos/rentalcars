4879f1f28410b5e48145e2e142b5ebc6568e2904
StylianosNikopoulos <nikstel2001@gmail.com>
2026-03-02 18:59:11 +0200

    remove unnecessary code

--- src/main/java/com/example/rentalcars/core/valueobject/Money.java
+++ src/main/java/com/example/rentalcars/core/valueobject/Money.java
@@ -9,8 +9,4 @@ public record Money(BigDecimal amount, String currency) {
         if (currency == null || currency.isBlank()) throw new IllegalArgumentException("Currency cannot be null or empty");
         amount = amount.setScale(2, RoundingMode.HALF_UP);
     }
-
-    public static Money euro(BigDecimal amount) {
-        return new Money(amount, "EUR");
-    }
 }
